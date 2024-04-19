967a185f04a87126c38f16565914e315f99a8096
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-04-19 12:50:33 +0200

    feat: implement private key repository

--- src/main/java/com/example/pki/PkiApplication.java
+++ src/main/java/com/example/pki/PkiApplication.java
@@ -2,14 +2,16 @@ package com.example.pki;
 
 import com.example.pki.repository.KeyStoreRepository;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import com.example.pki.repository.PrivateKeyRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import java.security.Security;
+import org.springframework.context.ApplicationContext;
+import java.security.*;
 
 @SpringBootApplication
 public class PkiApplication {
-
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(PkiApplication.class, args);
 		Security.addProvider(new BouncyCastleProvider());
