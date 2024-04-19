08cfbf0d91542b62ad37c70162879029bc11128a
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-04-19 12:02:57 +0200

    feat: certificate crud

--- src/main/java/com/example/pki/PkiApplication.java
+++ src/main/java/com/example/pki/PkiApplication.java
@@ -1,20 +1,18 @@
 package com.example.pki;
 
 import com.example.pki.repository.KeyStoreRepository;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import java.security.KeyStore;
+import java.security.Security;
 
 @SpringBootApplication
 public class PkiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(PkiApplication.class, args);
+		Security.addProvider(new BouncyCastleProvider());
 		KeyStoreRepository keyStoreRepository = (KeyStoreRepository) context.getBean("keyStoreRepository");
-		keyStoreRepository.readKeyStore(keyStoreRepository.keyStoreFileName, "123".toCharArray());
-		keyStoreRepository.writeKeyStore(keyStoreRepository.keyStoreFileName, "123".toCharArray());
 	}
-
 }
--- src/main/java/com/example/pki/repository/KeyStoreRepository.java
+++ src/main/java/com/example/pki/repository/KeyStoreRepository.java
@@ -1,12 +1,16 @@
 package com.example.pki.repository;
 
+import com.example.pki.model.Issuer;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
 import org.springframework.stereotype.Repository;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Enumeration;
 
 @Repository
 public class KeyStoreRepository {
@@ -57,5 +61,85 @@ public class KeyStoreRepository {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Issuer readIssuer(String keyStoreFile, String alias, String keyStorePassword, String keyPassword) {
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
+            keyStore.load(in, keyStorePassword.toCharArray());
+            Certificate cert = keyStore.getCertificate(alias);
+            PrivateKey privateKey = (PrivateKey) keyStore.getKey(alias, keyPassword.toCharArray());
+            X500Name issuerName = new JcaX509CertificateHolder((X509Certificate) cert).getSubject();
+            return new Issuer(privateKey, cert.getPublicKey(), issuerName);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Certificate readCertificate(String keyStoreFile, String keyStorePassword, String alias) {
+        try {
+            KeyStore ks = KeyStore.getInstance("JKS", "SUN");
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
+            ks.load(in, keyStorePassword.toCharArray());
+            if(ks.isCertificateEntry(alias)) {
+                java.security.cert.Certificate cert = ks.getCertificate(alias);
+                return cert;
+            }
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void writeCertificate(String alias, X509Certificate cert) {
+        try {
+            keyStore.setCertificateEntry(alias, cert);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteCertificate(String alias) {
+        try {
+            keyStore.deleteEntry(alias);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Certificate> getAllCertificates() {
+        ArrayList<Certificate> certs = new ArrayList<>();
+        try {
+            Enumeration<String> aliases = keyStore.aliases();
+            while (aliases.hasMoreElements()) {
+                String alias = aliases.nextElement();
+                Certificate cert = keyStore.getCertificate(alias);
+                certs.add(cert);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return certs;
     }
 }
