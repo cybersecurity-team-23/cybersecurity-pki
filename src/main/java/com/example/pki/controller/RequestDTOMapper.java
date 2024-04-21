9f158d855a56c872b19d0c8ebdfe11a018d70759
teodord25 <djuric.teodor25@gmail.com>
2024-04-21 14:36:08 +0200

    impl req dto mapper

---
+++ src/main/java/com/example/pki/controller/RequestDTOMapper.java
@@ -0,0 +1,39 @@
+package com.example.pki.controller;
+
+import com.example.pki.dto.RequestDTO;
+import com.example.pki.model.CertificateType;
+import com.example.pki.model.Request;
+
+public class RequestDTOMapper {
+    public static RequestDTO fromRequestToDTO(Request request) {
+        return new RequestDTO(
+                request.getId(),
+                request.getIssuerSerialNumber(),
+                request.getCommonName(),
+                request.getSurname(),
+                request.getGivenName(),
+                request.getOrganisation(),
+                request.getOrganisationalUnit(),
+                request.getCountry(),
+                request.getEmail(),
+                request.getType(),
+                request.getStatus()
+        );
+    }
+
+    public static Request fromDTOtoRequest(RequestDTO dto) {
+        return new Request(
+                dto.getId(),
+                dto.getIssuerSerialNumber(),
+                dto.getCommonName(),
+                dto.getSurname(),
+                dto.getGivenName(),
+                dto.getOrganisation(),
+                dto.getOrganisationalUnit(),
+                dto.getCountry(),
+                dto.getEmail(),
+                dto.getType(),
+                dto.getStatus()
+        );
+    }
+}
