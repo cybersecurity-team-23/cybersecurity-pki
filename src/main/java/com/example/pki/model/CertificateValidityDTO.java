d3fc4c6ba7a8ab8742136475fc9590f85fc4163c
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-04-22 10:05:39 +0200

    feat: impl validation endpoint

---
+++ src/main/java/com/example/pki/model/CertificateValidityDTO.java
@@ -0,0 +1,14 @@
+package com.example.pki.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CertificateValidityDTO {
+    private boolean isValid;
+}
